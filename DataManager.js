c76447ac9e74a8774a50a57e7c8b89d47149690e
Ryan O'Connor <ryanoconnor@Ryans-MacBook-Pro-3.local>
2017-07-11 13:12:22 -0400

    updated index and datamanger

--- DataManager.js
+++ DataManager.js
@@ -1,1 +1,41 @@
 // Hey
+
+console.log('yext.js loaded!')
+
+if(typeof(yext)=="undefined"){ // in case you'e already using an object called yext
+    yext={jobs:{}}
+}else{
+    yext.jobs={}
+}
+
+yext.getJSON=function(url){ // getJSON with promises
+    url = 'https://script.google.com/macros/s/AKfycbxH_t0MnnzTDvWnGKwpyIJUkJJqpuBOiZjwnerTgtGLsONojZg/exec?url='+encodeURIComponent(url)
+    return new Promise(function(resolve, reject) {
+        var s = document.createElement('script')
+        var uid = "job"+Date.now().toString()+Math.random().toString().slice(2)
+        s.id=uid
+        s.src=url+'&callback=yext.jobs.'+uid
+        yext.jobs[uid]=function(x){
+            resolve(x)
+            setTimeout(function(){  // remove the scripts that were successfull 
+                delete yext.jobs[uid]
+                var s = document.getElementById(uid)
+                s.parentElement.removeChild(s)
+            },1000)
+        }
+        s.onerror=function(){
+            reject({"error":('error loading '+url)})
+        }
+        document.head.appendChild(s)
+    })
+}
+
+yext.getJSON("https://api.yext.com/v2/accounts/[accountId]/locations?api_key=API_KEY&v=20170705") 
+.then (function(data){
+    console.log("SUCCESS")
+console.log(data)
+
+})
+.error (function(data) {
+console.log(data)
+})
