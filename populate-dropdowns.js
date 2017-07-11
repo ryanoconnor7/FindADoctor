d3511b4386d8f707d85843690c6cdf015d72d9a8
Ryan O'Connor <ryanoconnor@Ryans-MacBook-Pro-3.local>
2017-07-11 14:24:49 -0400

    added drop down data

---
+++ populate-dropdowns.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+$.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    //contentType: "application/json",
+    url: "https://findadoc.uhmc.sunysb.edu/sbmed/get-ins-lang.cfm",
+    data: {},
+    jsonpCallback: "pdCallback",
+	jsonp: false, 
+	success: function(data) {
+		for(var prop in data) {
+			if (data[prop].ORDER == '1') {
+	       		$('<option value=' + data[prop].CODE + '>' + data[prop].NAME + '</option>').appendTo('#Insure');
+			} else {
+				$('<option value=' + data[prop].CODE + '>' + data[prop].NAME + '</option>').appendTo('#Language');
+			}
+	    }	
+	},
+	error: function(xhr, status, errorThrown) {
+	//alert(errorThrown);
+    if (window.console) {
+    console.log("Error: " + errorThrown);
+	console.log("status: " + status);
+	console.dir(xhr);
+    }
+	},
+	complete: function(xhr, status) {
+	//console.log('finished');
+	}
+});
+});
+
