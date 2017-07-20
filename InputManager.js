2f7274d0c79bca724612dc6f7a015b3fa8dedeba
Ryan O'Connor <ryanoconnor@Ryans-MacBook-Pro-3.local>
2017-07-20 12:23:08 -0400

    make it better

--- InputManager.js
+++ InputManager.js
@@ -14,31 +14,32 @@ function mainTextFieldChanged() {
                 // additional.style="visibility:visible"
                 console.log("should show")
 
-                var specialtyURL = 'https://api.yext.com/v2/accounts/[accountId]/locationsearch?api_key=API_KEY&v=20170705' //[{%22specialties%22:{%22contains%22:[%22radiology%22]}}]' 
+                var url = 'https://api.yext.com/v2/accounts/[accountId]/locationsearch?api_key=API_KEY&v=20170705'
 
-                                $.getScript("DataManager.js").then(function() {
-                                        console.log("Making call...")  
-                                        makeAPICall(specialtyURL,'filters','[{"specialties":{"contains":["'+textField.value+'"]}}]')
-                                        .then(function(data) {
+                var uid = 'someUID'
 
-                                        try {
-                                        var json = JSON.parse(JSON.stringify(data))
-                                        console.log("JSON:", json)
-                                        handleSpecialtiesResponseSuccess(json)
-                                        } 
-                                        catch (e) {
-                                        console.log("error:", e)
-                                        }
-                                        })
-                                        .catch(function(error) {
-                                        console.log("error: ", error) 
+        }
+
+}
+
+//         $.getScript("DataManager.js").then(function() {
+        //                                 console.log("Making call...")  
+        //                                 makeAPICall(url,uid)
+        //                                 .then(function(data) {
+
+        //                                 try {
+        //                                 var json = JSON.parse(JSON.stringify(data))
+        //                                 console.log("JSON:", json)
+        //                                 handleSpecialtiesResponseSuccess(json)
+        //                                 } 
+        //                                 catch (e) {
+        //                                 console.log("error:", e)
+        //                                 }
+        //                                 })
+        //                                 .catch(function(error) {
+        //                                 console.log("error: ", error) 
                                         
-                                        })
-                                })
-                        }
-                }
-
- function handleSpecialtiesResponseSuccess(json) {
+        //                                 })
+        //                         })
 
 
- }
