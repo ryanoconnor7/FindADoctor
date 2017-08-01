66752251a7ab81f354dabe2dd47913b6440c5451
Ryan O'Connor <ryanoconnor@Ryans-MacBook-Pro-3.local>
2017-08-01 09:12:13 -0400

    updated package.json

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// // Create and Deploy Your First Cloud Functions// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
