76895736788d1a2bf31c51e3c7c20a75f8cc6513
Ryan O'Connor <ryanoconnor@Ryans-MacBook-Pro-3.local>
2017-08-03 14:25:31 -0400

    added more text labels

--- DataManager.js
+++ DataManager.js
@@ -58,3 +58,4 @@ function searchLocations(text) {
         resolve(searchResults)
     })
 }
+
--- InputManager.js
+++ InputManager.js
@@ -1,8 +1,12 @@
 
+var map = null
+var markers = []
+var statusDiv = null
 
 function mainTextFieldChanged() {
         var textField = document.getElementById("Query")
 
+        statusDiv = document.getElementById("status")
         var additional = document.getElementById("additional-fields")
 
         console.log("keyed up on main search field")
@@ -20,30 +24,103 @@ function mainTextFieldChanged() {
                         searchLocations(textField.value).then(function(results) {
 
                                 console.log("Received results ("+results.length+")")
-                                var statusLabel = document.getElementById("status")
-                                statusLabel.innerText = "Received results ("+results.length+")"
 
-                        })
+                                statusDiv.innerText = "Received results ("+results.length+")"
+                                statusDiv.innerHTML = ""
+                                if (markers.length > 0) {
+                                removeMarkers()
+
+                                }
+                                var bounds = new google.maps.LatLngBounds();
+
+                                for (i=0; i<=results.length-1; i++) {
+                                        var result = results[i]
+                                        var url = " "
+                                        if (result.headshot != "undefined" && result.headshot != null) {
+                                                if (result.headshot.url != "undefined" && result.headshot.url != null) {
+                                                      url = result.headshot.url
+
+                                                } else {
+                                                 url = result.logo.url
+                                                }
+                                        } else {
+                                              url = result.logo.url
+
+                                        }
+                                       
+                                        
+                                        var pos = {lat: Number(result.displayLat), lng: Number(result.displayLng)};
+                                        bounds.extend(pos);
+
+                                        var marker = new google.maps.Marker({
+                                        position: pos,
+                                        map: map,       
+                                        label: String(i+1),
+                                        animation: google.maps.Animation.DROP
+                                        });
+
+                                        marker.addListener('mouseover', function() {
+                                                // markerColorToWhite()  
+                                                var id = "cell" + i              
+                                                var cell = document.getElementsByClassName(i)
+
+                                                cell.style.backgroundColor = "9bcfef"
+                                        });     
+
+                                         statusDiv.innerHTML = statusDiv.innerHTML.concat(`
+                                        <div id="cell" class=${i} style="width:100%;height:90px;padding-top:5px;padding-bottom:5px;" action="javascript:toggleBounce(${i});">
+                                
+                                        <img align="left" class=headshot style="height:100%;width:auto;border-radius:10px;border:0px solid;" src=${url}></img>
+                                        
+                                        <h5 style="margin-top:0px;margin-bottom:-4px;font-family: 'Trebuchet MS',sans-serif">${result.firstName+" "+result.lastName}</h5>
+                                        <p style="line-spacing:0.1;font-family: 'Trebuchet MS',sans-serif;font-size:12px">${result.officeName}<br>${result.city}<p>
+                                        </div>
+                                        <div style="background-color:gray;height:1px;width:100%"></div>
+                                        `)
+                                                        
+                                        markers.push(marker)
+
+                                }
+                                //center the map to the geometric center of all markers
+                                map.panTo(bounds.getCenter())
+                                setActions()
+
+                                        
+                         })
                 })
         }
 }
 
-//  $.getScript("DataManager.js").then(function() {
-//                                         console.log("Making call...")  
-//                                         makeAPICall()
-//                                         .then(function(data) {
+ 
+ function initMap() {
 
-//                                         try {
-//                                         var json = JSON.parse(JSON.stringify(data))
-//                                         console.log("JSON:", json)
-//                                         handleSpecialtiesResponseSuccess(json)
-//                                         } 
-//                                         catch (e) {
-//                                         console.log("error:", e)
-//                                         }
-//                                         })
-//                                         .catch(function(error) {
-//                                         console.log("error: ", error) 
-                                        
-//                                         })
-//                                 })
+        var uluru = {lat: 40.915472, lng: -73.122721};
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+      
+ }
+
+ function removeMarkers() {
+        for (i=0;i<=markers.length-1;i++) {
+                markers[i].setMap(null)
+        }
+        markers = []
+ }
+
+ function markerColorToWhite() {
+         for (i=0;i<=statusDiv.childNodes.length-1;i++) {
+                var cell = statusDiv.childNodes[i]
+                cell.style.backgroundColor = "fff"
+
+        }
+ }
+
+ function toggleBounce(i) {
+        if (markers[i].getAnimation() !== null) {
+          markers[i].setAnimation(null);
+        } else {
+          markers[i].setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
